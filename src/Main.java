3a1f1d0edcdfe8a80f2c3afd5ad6f8d269e8b43b
Yuriy Sviridov <crip.shooter@gmail.com>
2022-09-05 19:14:36 +0300

    add lambda predicate

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
-
 public class Main {
+    private static final int MAX_AGE = 18;
     public static void main(String[] args) {
         List<Person> list = new ArrayList<>();
         list.add(new Person("Jack", "Black White Yellow Blue Pink Green", 52));
@@ -9,13 +9,17 @@ public class Main {
         list.add(new Person("Stevland", "Hardaway Morris Daniels", 72));
         list.add(new Person("Wayne","Rooney", 36));
         list.add(new Person("Cristiano","Ronaldo De Souza", 38));
+        list.add(new Person("Bill","Gates le Pascal", 13));
         list.add(new Person("Lionel","Messi Maradona",35));
         list.add(new Person("Lilly","Mai Robertson Gregory House", 42));
         list.add(new Person("Mohammed", "May", 25));
         list.add(new Person("Alba","Nichols",27));
+        list.add(new Person("Michael","Prascovia Elikolani Valiente", 10));
         list.add(new Person("Summer","Rose Livingston", 22));
         list.add(new Person("Neymar", "Da Silva Santos Junior", 30));
+        list.add(new Person("Carl","Johnson von Windows", 15));
         list.sort(new PersonComparator(3));
+        list.removeIf(person -> person.getAge() < MAX_AGE);
         System.out.println(list);
     }
 }
